4a4a0a6eb5093700223ea5b8693152be5d6b86b0
Maciek <macieq1989@gmail.com>
2025-08-15 11:16:06 +0100

    more changes

---
+++ src/util/prisma.ts
@@ -0,0 +1,3 @@
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient({ log: ['warn','error'] });
+process.on('beforeExit', async () => { try { await prisma.$disconnect(); } catch {} });
